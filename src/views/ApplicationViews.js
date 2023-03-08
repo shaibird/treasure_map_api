00bb87c0b3ac3320b3ca0f63fceab39aefccb675
Shaina Couch <shainacouch21@gmail.com>
2023-03-08 12:01:38 -0500

    initial commit

---
+++ src/views/ApplicationViews.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../components/auth/Login"
+import { Register } from "../components/auth/Register"
+import { Authorized } from "./Authorized"
+import { GameList } from "../components/game/GameList"
+import { EventList } from "../components/events/EventList"
+import { GameForm } from "../components/game/GameForm"
+import { EventForm } from "../components/events/EventForm"
+
+
+export const ApplicationViews = () => {
+    return <>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<Authorized />}>
+                <Route path="/games" element={<GameList />} />
+                <Route path="/events" element={<EventList />} />
+                <Route path="/games/new" element ={<GameForm />} />
+                <Route path="/event/new" element ={<EventForm />} />
+            </Route>
+        </Routes>
+    </>
+}
